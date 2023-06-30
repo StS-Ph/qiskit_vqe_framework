588af90262bcaaf7dae994e4f42e6d1d7115ee1f
StS-Ph <stephan.j.schuster@gmx.de>
2023-07-01 00:46:51 +0200

    Implemented unit tests for VQEEstimator classes

---
+++ tests/test_VQEEstimator.py
@@ -0,0 +1,177 @@
+import unittest
+from qiskit_ibm_runtime import QiskitRuntimeService, Session
+from qiskit import IBMQ
+import src
+import src.VQEEstimator as VQEE
+
+class TestVQEEstimatorCalibration(unittest.TestCase):
+    def setUp(self):
+        self.est_opt = {"transpilation_options": {"optimization_level": 3}, "backend_options": {"method": "automatic", "shots": 4000}, "run_options": {"shots": 1024}, "approximation": False, "skip_transpilation": False}
+        self.estimator_cal = VQEE.EstimatorCalibration(self.est_opt, "None", "None", "aer", "aer_automatic")
+
+    def test_repr(self):
+        self.assertEqual(repr(self.estimator_cal), "EstimatorCalibration(est_opt={}, noise_model_str=None, coupling_map_str=None, est_prim_str=aer, backend_str=aer_automatic)".format(self.est_opt))
+
+    def test_to_dict(self):
+        self.assertEqual(self.estimator_cal.to_dict(), {"name": "EstimatorCalibration", "noise_model_str": "None", "coupling_map_str": "None", "backend_str": "aer_automatic", "estimator_str": "aer", "estimator_options": self.est_opt})
+
+    def test_get_filevector(self):
+        header, data = self.estimator_cal.get_filevector()
+
+        self.assertEqual(header, ["estimator_str", "err_mitigation", "circ_optimization", "meas_shots", "backend_str", "noise_model", "coupling_map"])
+        self.assertEqual(data, ["aer", 0, 3, 1024, "aer_automatic", "None", "None"])
+    def test_validate_estimator_options_aer(self):
+        est_prim_str = "aer"
+        est_opt = {"transpilation_options": {"optimization_level": 3}, "backend_options": {"method": "automatic", "shots": 4000}, "approximation": False, "skip_transpilation": False, "run_options": {"shots": 1024}}
+        self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+
+        est_opt = {"run_options": None}
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"transpilation_options": None, "backend_options": None, "approximation": False, "skip_transpilation": False, "run_options": None}
+
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+        self.assertEqual(val_est_opt["transpilation_options"], {"optimization_level": 0})
+        self.assertEqual(val_est_opt["backend_options"], {})
+        self.assertEqual(val_est_opt["run_options"], {"shots": 1024})
+
+        est_opt = {"transpilation_options": {"optimization_level": None}, "backend_options": {"method": "automatic", "shots": 4000}, "approximation": False, "skip_transpilation": False, "run_options": {"shots": 1024}}
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+        self.assertEqual(val_est_opt["transpilation_options"]["optimization_level"], 0)
+        est_opt["skip_transpilation"] = True
+        est_opt["transpilation_options"]["optimization_level"] = 1
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"transpilation_options": {"optimization_level": None}, "backend_options": {"method": "automatic", "shots": None}, "approximation": False, "skip_transpilation": False, "run_options": {}}
+
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+        self.assertEqual(val_est_opt["backend_options"]["shots"], 1024)
+
+        est_opt = {"transpilation_options": {"optimization_level": None}, "backend_options": {"method": "automatic"}, "approximation": False, "skip_transpilation": False, "run_options": {"shots": None}}
+
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+        self.assertEqual(val_est_opt["run_options"]["shots"], 1024)
+
+        est_opt = {"transpilation_options": {"optimization_level": None}, "backend_options": {"method": "automatic"}, "approximation": False, "skip_transpilation": False, "run_options": {}}
+
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+        self.assertEqual(val_est_opt["run_options"]["shots"], 1024)
+
+    def test_validate_estimator_options_ibm_runtime(self):
+        est_prim_str = "ibm_runtime"
+        est_opt = {"run_options": None}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"optimization_level": None, "resilience_level": None, "max_execution_time": None, "transpilation_options": {}, "resilience_options": {}, "execution_options": {}, "enviroment_options": {}, "simulator_options": {}}
+
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+
+        self.assertEqual(val_est_opt["resilience_level"], 0)
+        self.assertEqual(val_est_opt["optimization_level"], 0)
+
+        est_opt = {"optimization_level": None, "resilience_level": None, "max_execution_time": None, "transpilation_options": None, "resilience_options": {}, "execution_options": {}, "enviroment_options": {}, "simulator_options": {}}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"optimization_level": None, "resilience_level": None, "max_execution_time": None, "transpilation_options": {}, "resilience_options": None, "execution_options": {}, "enviroment_options": {}, "simulator_options": {}}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"optimization_level": None, "resilience_level": None, "max_execution_time": None, "transpilation_options": {}, "resilience_options": {}, "execution_options": None, "enviroment_options": {}, "simulator_options": {}}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"optimization_level": None, "resilience_level": None, "max_execution_time": None, "transpilation_options": {}, "resilience_options": {}, "execution_options": {}, "enviroment_options": None, "simulator_options": {}}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"optimization_level": None, "resilience_level": None, "max_execution_time": None, "transpilation_options": {}, "resilience_options": {}, "execution_options": {}, "enviroment_options": {}, "simulator_options": None}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"optimization_level": None, "resilience_level": None, "max_execution_time": None, "transpilation_options": {}, "resilience_options": {}, "execution_options": {}, "enviroment_options": {}, "simulator_options": {}}
+
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+
+        self.assertEqual(val_est_opt["execution_options"]["shots"], 1024)
+
+    def test_validate_estimator_options_terra(self):
+        est_prim_str = "terra"
+        est_opt = {"backend_options": None}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+        est_opt = {"run_options": None}
+        val_est_opt = self.estimator_cal._validate_estimator_options(est_opt, est_prim_str)
+
+        self.assertEqual(val_est_opt["run_options"], {})
+
+        est_opt = {"run_options": 1}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+        
+    def test_validate_estimator_options_ion_trap(self):
+        est_prim_str = "ion_trap"
+        est_opt = {}
+
+        self.assertRaises(NotImplementedError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+
+    def test_validate_estimator_options_est_prim_str(self):
+        est_prim_str = "unkown_estimator"
+        est_opt = {}
+
+        self.assertRaises(ValueError, self.estimator_cal._validate_estimator_options, est_opt, est_prim_str)
+
+
+class TestVQEEstimator(unittest.TestCase):
+    def setUp(self):
+        self.est_opt = {"transpilation_options": {"optimization_level": 3}, "backend_options": {"method": "automatic", "shots": 4000}, "run_options": {"shots": 1024}, "approximation": False, "skip_transpilation": False}
+        self.estimator_cal = VQEE.EstimatorCalibration(self.est_opt, "None", "None", "aer", "aer_automatic")
+        self.vqe_estimator = VQEE.VQEEstimator(self.estimator_cal)
+
+
+    def test_init(self):
+        est_opt = {"optimization_level": 0, "resilience_level": 0, "max_execution_time": None, "transpilation_options": {}, "resilience_options": {}, "execution_options": {}, "enviroment_options": {}, "simulator_options": {}}
+        
+        est_cal = VQEE.EstimatorCalibration(est_opt, "None", "None", "ibm_runtime", "ibm_runtime_qasm")
+
+        self.assertRaises(ValueError, VQEE.VQEEstimator, est_cal)
+
+    def test_repr(self):
+        self.assertEqual(repr(self.vqe_estimator), "VQEEstimator(estimator_parameters={}, session={})".format(self.vqe_estimator.parameters, self.vqe_estimator.session))
+
+    def test_to_dic(self):
+        self.assertEqual(self.vqe_estimator.to_dict(), {"parameters": self.vqe_estimator.parameters.to_dict(), "session": self.vqe_estimator.session, "estimator": self.vqe_estimator.estimator})
+
+    def test_parameters_setter(self):
+        new_est_opt = {"transpilation_options": {"optimization_level": 0}, "backend_options": {"method": "automatic", "shots": 4000}, "run_options": {}, "approximation": False, "skip_transpilation": False}
+        est_cal = VQEE.EstimatorCalibration(new_est_opt, "None", "None", "aer", "aer_automatic")
+        self.vqe_estimator.parameters = est_cal
+
+        print("Aer Estimator object:")
+        print(self.vqe_estimator.estimator)
+        self.assertEqual(self.vqe_estimator.parameters.estimator_options, new_est_opt)
+    def test_get_estimator_ibm_runtime(self):
+
+        est_opt = {"optimization_level": 1, "resilience_level": 0, "max_execution_time": None, "transpilation_options": {}, "resilience_options": {}, "execution_options": {"shots": 4000}, "enviroment_options": {}, "simulator_options": {}}
+        backend_str = "ibmq_qasm_simulator"
+        est_cal = VQEE.EstimatorCalibration(est_opt, "None", "None", "ibm_runtime", backend_str)
+        service = QiskitRuntimeService()
+        
+        
+        with Session(service=service, backend=backend_str) as session:
+            vqe_est = VQEE.VQEEstimator(est_cal, session=session)
+
+            print("ibm runtime Estimator object:")
+            print(vqe_est.estimator)
+
+    def test_get_estimator_terra(self):
+        est_opt = {"run_options": {"shots": None}}
+        est_cal = VQEE.EstimatorCalibration(est_opt, "None", "None", "terra", "statevector")
+        vqe_est = VQEE.VQEEstimator(est_cal)
+
+        print("terra Estimator object:")
+        print(vqe_est.estimator)
