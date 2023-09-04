168b8e9fb65dcc0eafd34f3532d695c4e3f7ca9c
StS-Ph <stephan.j.schuster@gmx.de>
2023-09-04 16:14:57 +0200

    added inference result class

--- qiskit_vqe_framework/VQEResult.py
+++ qiskit_vqe_framework/VQEResult.py
@@ -170,3 +170,81 @@ class VQEResult:
             data.extend(curr_data)
         
         return header, data
+
+class InferenceResult:
+    def __init__(self,
+                 inference_data: ResultData,
+                 inference_cal_list: List[cal.Calibration],
+                 vqe_result: VQEResult,
+                 metadata: Dict) -> None:
+        self._data = inference_data
+        self._calibration_list = inference_cal_list
+        self._vqe_reference = vqe_result
+        self._metadata = metadata
+
+    @property
+    def calibration_list(self):
+        return self._calibration_list
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def vqe_reference(self):
+        return self._vqe_reference
+
+    @property
+    def metadata(self):
+        return self._metadata
+
+    def __repr__(self):
+        out = "InferenceResult(inference_data={}, inference_cal_list={}, vqe_result={}, metadata={})".format(self._data, self._calibration_list, self._vqe_reference, self._metadata)
+        return out
+    
+    def to_dict(self):
+        result_dict = self._data.to_dict()
+        cnt = 0
+        for cal in self._calibration_list:
+            cal_dict = cal.to_dict()
+            cal_name = cal_dict.pop("name", None)
+            if cal_name is None:
+                cal_name = "unknown_cal"+str(cnt)
+                cnt += 1
+            result_dict[cal_name] = cal_dict
+
+        result_dict["vqe_reference"] = self._vqe_reference.to_dict()
+        result_dict["metadata"] = self._metadata
+
+        return result_dict
+
+    def get_filevector(self):
+        """
+        Define method to write the summarized (shorted) result in a list format
+
+        output: (header, data)
+        """
+        header = []
+        data = []
+
+        for cal in self._calibration_list:
+            cal_header, cal_data = cal.get_filevector()
+            header.extend(cal_header)
+            data.extend(cal_data)
+
+        vqe_ref_data_no_angles = copy.deepcopy(self._vqe_reference.data)
+        #print(vqe_ref_data_no_angles)
+        # remove double display of angles in filevector
+        if hasattr(self._data, "angles"):
+            delattr(vqe_ref_data_no_angles, "angles")
+        curr_header, curr_data = vqe_ref_data_no_angles.get_filevector()
+        curr_header = [s + "_vqe" for s in curr_header]
+        header.extend(curr_header)
+        data.extend(curr_data)
+
+        curr_header, curr_data = self._data.get_filevector()
+        curr_header = [s + "_infer" for s in curr_header]
+        header.extend(curr_header)
+        data.extend(curr_data)
+        
+        return header, data
